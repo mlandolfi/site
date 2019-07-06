98a029af611f69dc70b84fca7bb298edba564837
Mike Landolfi <mlandolfi99@gmail.com>
2019-07-06 12:15:37 -0700

    created tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import Nav from './components/Nav'
+
+const tabs = [
+	{ label: 'about', styleId: 'nav-first', color: '#ff652f' },
+	{ label: 'resume', styleId: 'nav-second', color: '#ffe400' },
+	{ label: 'projects', styleId: 'nav-third', color: '#14a76c' },
+]
+
+function App(props) {
+	const [tab, setTab] = useState(tabs[parseInt(tabs.length/2)].label);
+	const [tabColor, setTabColor] = useState(tabs[parseInt(tabs.length/2)].color)
+
+	const handleNavClick = (label, color) => {
+		setTab(label);
+		setTabColor(color);
+	}
+
 	return (
-		<div className="App" >
+		<div
+			className="app-root"
+		>
+			<div
+				className="nav-header"
+			>
+				{tabs.map((navTab) => (
+					<Nav
+						key={`nav-${navTab.label}`}
+						styleId={navTab.styleId}
+						label={navTab.label}
+						color={navTab.color}
+						onClick={handleNavClick}
+						selected={tab===navTab.label}
+					/>
+					))}
+			</div>
+			<div className="blob" />
 		</div>
 	);
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css'
+
+export default function Nav(props) {
+	return (
+		<div
+			className="nav-root"
+			id={props.styleId}
+			onClick={() => props.onClick(props.label, props.color)}
+			style={props.selected ? { color: '#fff' } : {}}
+		>
+			{props.label}
+		</div>
+		)
+}
