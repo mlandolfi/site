7a30c94b9affe4166705da3dccb3e4541e1fa7d0
Michael Landolfi <mlandolfi99@gmail.com>
2023-08-19 20:34:47 -0700

    welcome page

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import cx from "classnames";
+
 import styles from "./Welcome.module.css";
+import {
+  handleForDeleteIntro,
+  handleForIntro,
+  handleForWIP,
+  handleForRemoveAbout,
+  handleForWelcomeBanner,
+  handleForWriteAbout,
+} from "./handlers";
 
 export const Welcome = (): JSX.Element => {
+  const lastScroll = React.useRef(0);
+
+  const scrollHandler = useCallback(() => {
+    const currentScroll = window.scrollY;
+    handleForWelcomeBanner(currentScroll, lastScroll.current);
+    handleForIntro(currentScroll, lastScroll.current);
+    handleForDeleteIntro(currentScroll, lastScroll.current);
+    handleForWriteAbout(currentScroll, lastScroll.current);
+    handleForRemoveAbout(currentScroll, lastScroll.current);
+    handleForWIP(currentScroll, lastScroll.current);
+    lastScroll.current = currentScroll;
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [scrollHandler]);
+
   return (
     <div className={styles.root}>
-      <h1 className={styles.header}>Welcome</h1>
+      <div className={styles.bannerContainer}>
+        <span
+          className={cx(styles.welcomeBanner, styles.banner)}
+          id="welcome-banner"
+        >
+          Welcome
+        </span>
+        <span
+          className={cx(styles.introBanner, styles.banner)}
+          id="intro-banner"
+        >
+          I'm Mike
+        </span>
+        <span className={cx(styles.wipBanner, styles.banner)} id="wip-banner">
+          It's still a work in progress...
+        </span>
+      </div>
     </div>
   );
 };
---
+++ src/views/Welcome/handlers.ts
@@ -0,0 +1,209 @@
+const getPartitionIndexes = ({
+  currentScroll,
+  lastScroll,
+  partitions,
+}: {
+  currentScroll: number;
+  lastScroll: number;
+  partitions: number[];
+}): {
+  currentPartitionIndex: number;
+  lastPartitionIndex: number;
+  percentThrough: number;
+} => {
+  let currentPartitionIndex = -1,
+    lastPartitionIndex = -1;
+  for (let i = 0; i < partitions.length; i++) {
+    if (currentScroll < partitions[i] && currentPartitionIndex === -1) {
+      currentPartitionIndex = i - 1;
+    }
+    if (lastScroll < partitions[i] && lastPartitionIndex === -1) {
+      lastPartitionIndex = i - 1;
+    }
+  }
+
+  return {
+    currentPartitionIndex,
+    lastPartitionIndex,
+    percentThrough: currentPartitionIndex / partitions.length,
+  };
+};
+
+// ============================================================ //
+//                         Handlers                             //
+//==============================================================//
+
+const welcomeBannerPartitions = Array.from({ length: 90 }, (_, i) => i * 10);
+export const handleForWelcomeBanner = (
+  currentScroll: number,
+  lastScroll: number
+) => {
+  const { currentPartitionIndex, lastPartitionIndex } = getPartitionIndexes({
+    currentScroll,
+    lastScroll,
+    partitions: welcomeBannerPartitions,
+  });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const header = document.getElementById("welcome-banner");
+    if (header) {
+      if (currentPartitionIndex === -1) {
+        header.style.display = "none";
+        return;
+      }
+      header.style.display = "block";
+
+      header.style.transform = `rotateX(${
+        currentPartitionIndex * (90 / welcomeBannerPartitions.length)
+      }deg) translateY(-${
+        currentPartitionIndex * (200 / welcomeBannerPartitions.length)
+      }px) translateZ(${
+        currentPartitionIndex * (150 / welcomeBannerPartitions.length)
+      }px)`;
+    }
+  }
+};
+
+const introPartitions = Array.from({ length: 90 }, (_, i) => i * 10 + 500);
+export const handleForIntro = (currentScroll: number, lastScroll: number) => {
+  const { currentPartitionIndex, lastPartitionIndex } = getPartitionIndexes({
+    currentScroll,
+    lastScroll,
+    partitions: introPartitions,
+  });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const intro = document.getElementById("intro-banner");
+    if (intro) {
+      if (currentPartitionIndex === -1) {
+        return;
+      }
+      intro.style.display = "flex";
+
+      intro.style.transform = `rotateX(${
+        270 + (currentPartitionIndex * 90) / introPartitions.length
+      }deg) translateY(${
+        (introPartitions.length - currentPartitionIndex) *
+        (200 / introPartitions.length)
+      }px) translateZ(${
+        (introPartitions.length - currentPartitionIndex) *
+        (200 / introPartitions.length)
+      }px)`;
+    }
+  }
+};
+
+const introString = " I'm Mike";
+const deleteIntroPartitions = Array.from(
+  { length: 30 },
+  (_, i) => i * 30 + 1600
+);
+export const handleForDeleteIntro = (
+  currentScroll: number,
+  lastScroll: number
+) => {
+  const { currentPartitionIndex, lastPartitionIndex } = getPartitionIndexes({
+    currentScroll,
+    lastScroll,
+    partitions: deleteIntroPartitions,
+  });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const intro = document.getElementById("intro-banner");
+    if (intro) {
+      if (currentPartitionIndex === -1) {
+        return;
+      }
+      intro.style.display = "flex";
+
+      const remainingIntro = introString.slice(
+        0,
+        Math.floor(
+          ((deleteIntroPartitions.length - currentPartitionIndex) /
+            deleteIntroPartitions.length) *
+            introString.length
+        )
+      );
+      intro.innerText = remainingIntro;
+    }
+  }
+};
+
+const writeAboutPartitions = Array.from(
+  { length: 50 },
+  (_, i) => i * 20 + 2500
+);
+const writeAboutString = "This is my website   ";
+export const handleForWriteAbout = (
+  currentScroll: number,
+  lastScroll: number
+) => {
+  const { currentPartitionIndex, lastPartitionIndex } = getPartitionIndexes({
+    currentScroll,
+    lastScroll,
+    partitions: writeAboutPartitions,
+  });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const intro = document.getElementById("intro-banner");
+    if (intro) {
+      if (currentPartitionIndex === -1) {
+        return;
+      }
+      intro.style.display = "flex";
+
+      const writeAbout = writeAboutString.slice(
+        0,
+        Math.floor(
+          (currentPartitionIndex / writeAboutPartitions.length) *
+            writeAboutString.length
+        )
+      );
+      intro.innerText = writeAbout;
+    }
+  }
+};
+
+const removeAboutPartitions = Array.from(
+  { length: 90 },
+  (_, i) => i * 10 + 3700
+);
+export const handleForRemoveAbout = (
+  currentScroll: number,
+  lastScroll: number
+) => {
+  const { currentPartitionIndex, lastPartitionIndex, percentThrough } =
+    getPartitionIndexes({
+      currentScroll,
+      lastScroll,
+      partitions: removeAboutPartitions,
+    });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const intro = document.getElementById("intro-banner");
+    if (intro) {
+      if (currentPartitionIndex === -1) {
+        return;
+      }
+      intro.style.display = "flex";
+
+      intro.style.transform = `translateY(-${percentThrough * 400}px)`;
+    }
+  }
+};
+
+const wipPartitions = Array.from({ length: 90 }, (_, i) => i * 10 + 3750);
+export const handleForWIP = (currentScroll: number, lastScroll: number) => {
+  const { currentPartitionIndex, lastPartitionIndex, percentThrough } =
+    getPartitionIndexes({
+      currentScroll,
+      lastScroll,
+      partitions: wipPartitions,
+    });
+  if (currentPartitionIndex !== lastPartitionIndex) {
+    const intro = document.getElementById("wip-banner");
+    if (intro) {
+      if (currentPartitionIndex === -1) {
+        return;
+      }
+      intro.style.display = "flex";
+
+      intro.style.transform = `translateX(-${(1 - percentThrough) * 2000}px)`;
+    }
+  }
+};
